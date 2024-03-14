df4c20a328568bebeef9938827417a1bb97bcea3
GaganBassi <gaganbassi12@gmail.com>
2024-03-14 18:05:28 -0400

    Addition and removal of file with process.argv

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 module.exports={
     x:10
 }
+
+const fs=require('fs');
+const inp=process.argv;//it will give the input whatever we pass while executing this, for example, node app.js 'Apple' 'Grapes'
+//process.argv has array in element starts from 2nd index will take this input
+//here inp[2]='Apple' and inp[3]='Grapes'
+
+console.log(inp);
+
+//fs.writeFileSync(/* file_name , text inside the file*/ inp[2],inp[3]);
+
+if(inp[2]=='add'){
+    fs.writeFileSync(/* file_name , text inside the file*/ inp[3],inp[4]);
+}
+else if(inp[2]=='remove'){
+    fs.unlinkSync(inp[3]);
+}
+else{
+    console.log('Enter valid ist argument');
+}
--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const colors=require('colors');
 console.log(app);
 console.log("Test".red);
 //colors package is used to change the color of the text
+
