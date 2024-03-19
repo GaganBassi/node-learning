bac942fc21cad219f4de5890c3b026b0c3d3f9ad
GaganBassi <gaganbassi12@gmail.com>
2024-03-19 14:47:05 -0400

    CoderDost Esmodule, export and import

--- Coder_Dost/data1.js
+++ Coder_Dost/data1.js
@@ -4,5 +4,9 @@ const add=(a,b)=>{
 const b=2;
 //console.log("hello")
 //module.exports={sum:add, a:2};
-exports.add=add;
-exports.b=b;
+//exports.add=add;
+//exports.b=b;
+
+//Trying with the ESModule latest type.
+
+export {add,b};
--- Coder_Dost/index1.js
+++ Coder_Dost/index1.js
@@ -1,3 +1,8 @@
-const sum1=require('./data1');
+//const sum1=require('./data1');
 //console.log(sum.add(2,3));//along sum will give the object of exports.
-console.log(sum1);
+//console.log(sum1);
+
+//Trying latest ESModule type
+
+import {add,b} from './data1.js'
+console.log(add(1,2));
