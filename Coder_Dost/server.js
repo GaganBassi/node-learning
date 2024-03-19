7d147ed7cad4a2984dba487d0f07f9e522628d02
GaganBassi <gaganbassi12@gmail.com>
2024-03-19 16:22:08 -0400

    Creation of static file

--- Coder_Dost/server.js
+++ Coder_Dost/server.js
@@ -2,9 +2,16 @@ const http=require('http');
 //console.log(http);
 const data={"Name":"Gagan"};
 
+const fs=require('fs');
+const dataFile=fs.readFileSync('index.html','utf-8');
+
 const server=http.createServer((req,res)=>{
+    //console.log(req.url);//to get the searched URl
     res.setHeader('Dummy','value');
-    res.setHeader('content-type','application/JSON');
-    res.end(JSON.stringify(data));
+    //res.setHeader('content-type','application/JSON');
+    //res.end(JSON.stringify(data));
+    res.setHeader('content-type','text/html')//type of response data without this it will treat this as a normal text and print tags 
+    //on the browser as a resposnse.
+    res.end(dataFile);
 })
 server.listen(8000);
