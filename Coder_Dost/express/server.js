3f9552add8008e5ce01b04ec4aaea4177e551975
GaganBassi <gaganbassi12@gmail.com>
2024-03-19 18:17:45 -0400

    Methods routes

--- Coder_Dost/express/server.js
+++ Coder_Dost/express/server.js
@@ -4,12 +4,23 @@ const app=express();
 
 app.get('/',(req,res)=>{
     
-    res.send(' <h1>First Server</h1>');
+    res.json({'type':'Get'});
+    //res.send(' <h1>First Server</h1>');
     //res.status(201).send('')//To send status along with this.
     //res.sendStatus(404);
 })
 
-
+//To use these operations, use either thunderclient or postman.
+app.post('/',(req,res)=>{
+    res.json({'type':'Post'});
+});
+app.put('/',(req,res)=>{
+    res.json({'type':'Put'});
+})
+app.delete('/',(req,res)=>{
+    res.json({'type':'delete'});
+})
+app.patch('/',(req,res)=>{res.json({'type':'patch'})})
 //app.listen should be at the end.
 app.listen(8002,()=>{
     console.log('Server Started');
