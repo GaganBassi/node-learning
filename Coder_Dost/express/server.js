3b24a309543712c156853add632e7793ee79dd23
GaganBassi <gaganbassi12@gmail.com>
2024-03-19 18:06:49 -0400

    send response using route

--- Coder_Dost/express/server.js
+++ Coder_Dost/express/server.js
@@ -1,6 +1,16 @@
 const express=require('express');
 
-const server=express();
-server.listen(8002,()=>{
+const app=express();
+
+app.get('/',(req,res)=>{
+    
+    res.send(' <h1>First Server</h1>');
+    //res.status(201).send('')//To send status along with this.
+    //res.sendStatus(404);
+})
+
+
+//app.listen should be at the end.
+app.listen(8002,()=>{
     console.log('Server Started');
 });
