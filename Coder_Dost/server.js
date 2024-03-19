9854d05585aff9d50dca30ea8b609dbb25efb36b
GaganBassi <gaganbassi12@gmail.com>
2024-03-19 16:39:09 -0400

    Routes in https

--- Coder_Dost/server.js
+++ Coder_Dost/server.js
@@ -7,13 +7,27 @@ const fs=require('fs');
 const dataJson=fs.readFileSync('data.json','utf-8');
 
 const server=http.createServer((req,res)=>{
+//Routes in https
+    switch(req.url){
+        case '/':
+            res.setHeader('Dummy','value');
+            res.end('Home Page');
+            break;
+        case '/api':
+            res.setHeader('content-type','application/JSON');
+            res.end(dataJson);
+            break;
+        default:
+            res.writeHead(404);
+            res.end();
+
+    }
     //console.log(req.url);//to get the searched URl
-    res.setHeader('Dummy','value');
+    //res.setHeader('Dummy','value');
     //res.setHeader('content-type','application/JSON');
     //res.end(JSON.stringify(data));
     //res.setHeader('content-type','text/html')//type of response data without this it will treat this as a normal text and print tags 
     //on the browser as a resposnse.
-    res.setHeader('content-type','application/JSON');
-    res.end(dataJson);
+    
 })
 server.listen(8000);
