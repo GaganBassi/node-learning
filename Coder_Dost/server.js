599265f3b29aecec9f95cf5c5b4555ccf0cb27cf
GaganBassi <gaganbassi12@gmail.com>
2024-03-19 16:27:54 -0400

    Sending JSON

--- Coder_Dost/server.js
+++ Coder_Dost/server.js
@@ -3,15 +3,17 @@ const http=require('http');
 const data={"Name":"Gagan"};
 
 const fs=require('fs');
-const dataFile=fs.readFileSync('index.html','utf-8');
+//const dataFile=fs.readFileSync('index.html','utf-8');
+const dataJson=fs.readFileSync('data.json','utf-8');
 
 const server=http.createServer((req,res)=>{
     //console.log(req.url);//to get the searched URl
     res.setHeader('Dummy','value');
     //res.setHeader('content-type','application/JSON');
     //res.end(JSON.stringify(data));
-    res.setHeader('content-type','text/html')//type of response data without this it will treat this as a normal text and print tags 
+    //res.setHeader('content-type','text/html')//type of response data without this it will treat this as a normal text and print tags 
     //on the browser as a resposnse.
-    res.end(dataFile);
+    res.setHeader('content-type','application/JSON');
+    res.end(dataJson);
 })
 server.listen(8000);
