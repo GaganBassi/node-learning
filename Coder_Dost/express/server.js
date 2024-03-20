5071b4d016921cf7fe6939375c96686e5715b051
GaganBassi <gaganbassi12@gmail.com>
2024-03-20 14:32:24 -0400

    Important learning of POST request, do it carefully.

--- Coder_Dost/express/server.js
+++ Coder_Dost/express/server.js
@@ -2,6 +2,8 @@ const express=require('express');
 
 const app=express();
 
+const fs=require('fs');
+const data2=fs.readFileSync('index.html','utf-8');
 //app.use() will execute before every request.
 //Creation of middleware between client and server.
 /**app.use((req,res,next)=>{
@@ -14,6 +16,24 @@ const app=express();
     }
 //next();//here next means request can move down from this middleware
 })**/
+
+//express.json() is a middleware
+app.use(express.json());//Express will read the body of the request, otherwise req.body will not work.
+app.use(express.urlencoded({ extended: true }));//To fetch post data from form
+const authPost=(req,res,next)=>{//Actual creation of middleware
+    console.log(req.method,req.ip, req.hostname,new Date, req.get('User-Agent'));
+    if(req.body.postRequest==='Gagan'){//For post request data will come from request body and it should be checked from postman
+        next();
+    }
+    else{
+        res.sendStatus(404);
+    }
+//next();//here next means request can move down from this middleware
+}
+
+/*Send body as a raw and type json in POSTMAN to check this.{
+    "pass":"123"
+}*/
 
 const auth=(req,res,next)=>{//Actual creation of middleware
     console.log(req.method,req.ip, req.hostname,new Date, req.get('User-Agent'));
@@ -41,7 +61,8 @@ app.get('/',auth,(req,res)=>{//Now only this get method has the auth middleware 
 
 
 //These are called API-Endpoints-route
-app.post('/',(req,res)=>{//now these methods are not part of auth middleware as we removed app.use() and applied auth on get request only.
+
+app.post('/',authPost,(req,res)=>{//authPost middleware is for POST request.
     res.json({'type':'Post'});
 });
 app.put('/',(req,res)=>{
@@ -51,6 +72,14 @@ app.delete('/',(req,res)=>{
     res.json({'type':'delete'});
 })
 app.patch('/',(req,res)=>{res.json({'type':'patch'})})
+
+app.get('/form',(req,res)=>{
+res.send(data2);
+})
+app.post('/form',authPost,(req,res)=>{
+    console.log(req.body);
+    res.send('Hell');
+})
 //app.listen should be at the end.
 app.listen(8002,()=>{
     console.log('Server Started');
